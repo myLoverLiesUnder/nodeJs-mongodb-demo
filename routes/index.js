a030b83ffdeb5fe1b22a6cf5d4ca9afec257376c
gezx <1003181892@qq.com>
2018-10-22 12:02:40 +0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var Vote = require("../database/vote");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    Vote.find(function (err, data) {
+        res.render('index', {title: 'Vote Web', data: data});
+    })
 });
-
 
 module.exports = router;
